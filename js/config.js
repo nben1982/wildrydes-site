4a713cfc6dd457b323ca57071fd5724fe9fe8159
nben1982 <ben.alaya.naim@gmail.com>
2025-10-27 17:30:48 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-3_ypV3k86bh', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5khucll92oe4c3iedlot936r7l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-3' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
